101012c98f7b70df0528f5a530d625760a32389e
Caleb Jones <bionicle.dragon@gmail.com>
2015-08-22 09:04:08 -0400

    Add player
    Add player motion script
    Move test scene into scene folder
    Create folders for scenes, prefabs, and scripts

---
+++ Bites/Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotion : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        var motion = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.position += (Vector3) motion * Time.deltaTime * speed;
+    }
+}
